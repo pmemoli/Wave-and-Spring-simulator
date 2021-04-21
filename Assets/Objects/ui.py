bc7655036f901ca3cb44678de705041860b0310a
Pedro-Memoli-Buffa <72678454+Pedro-Memoli-Buffa@users.noreply.github.com>
2021-04-21 02:28:51 -0300

    Add files via upload

--- Assets/Objects/ui.py
+++ Assets/Objects/ui.py
@@ -1,21 +1,18 @@
 from tkinter import *
 
 instructions = """
-En:
-Press right click to set a mass, left click to connect two masses with a spring, 
-left click + R to create a rod approximation (k ~ 10000 and initial length = natural length)
-and middle click to create an unmovable mass. ENTER starts/pauses the simulation.
-BACKSPACE deletes current configuration. Press Q to set the physical properties 
-of the next mass and/or string. Once created the properties remain, so concatenation
-of different masses and springs is possible. Press G to turn gravity ON/OFF
+Press left click to set a mass, right click to connect two masses with a spring,
+right click + R to create a rod approximation and middle click to create an "unmovable" mass.
+ENTER starts/pauses the simulation. BACKSPACE deletes current configuration. Press Q to set
+the physical properties for the next mass and/or string. Once created the properties remain,
+so concatenation of different masses and springs is possible. Press G to turn gravity ON/OFF
 
-Esp:
-Presionar click derecho para crear una masa, click izquierdo para conectar dos masas con un resorte,
-click izquierdo + R para conectar dos masas por una aproximacion de vara (resorte con k muy grande de largo inicial lo),
-y click medio (la ruedita) para crear una masa que no se mueve (masa ~10000kg). ENTER empieza/pausa la simulacion.
-Se puede borrar la configuracion con BACKSPACE. Presionar Q permite cambiar las propiedades fisicas
-de los proximos objetos. Una vez creados estas propiedades permanecen, de forma que se pueden combinar
-masas y resortes distintos. La G activa o desactiva la gravedad.
+Presionar click izquierdo para crear una masa, click derecho para conectar dos masas con un resorte,
+click derecho + R para crear una aproximacion de vara y click medio (la ruedita) para crear una masa
+que no se mueve. ENTER empieza/pausa la simulacion. Se puede borrar la configuracion con BACKSPACE.
+Presionar Q permite cambiar las propiedades fisicas de los proximos objetos. Una vez creados estas
+propiedades permanecen, de forma que se pueden combinar masas y resortes distintos.
+La G activa o desactiva la gravedad.
 
 40 Pixels = 1 Meter, g = 40pixels/s^2
 """
